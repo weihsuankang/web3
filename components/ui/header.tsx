6bd3438406c4d37a79ee02cd689a7467b5b1df1f
weihsuankang <sammy00024@gmail.com>
2024-10-10 23:08:05 +0800

    change language

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
                 href="/signin"
                 className="btn-sm relative bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] py-[5px] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]"
               >
-                Sign In
+                登入
               </Link>
             </li>
             <li>
@@ -28,7 +28,7 @@ export default function Header() {
                 href="/signup"
                 className="btn-sm bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] py-[5px] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%]"
               >
-                Register
+                註冊
               </Link>
             </li>
           </ul>
