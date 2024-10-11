ae103fe9a97ca3ecfae0abbd143191aa5d9a2f33
weihsuankang <sammy00024@gmail.com>
2024-10-11 22:23:06 +0800

    change language of sign up

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -13,7 +13,7 @@ export default function SignUp() {
           {/* Section header */}
           <div className="pb-12 text-center">
             <h1 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-              Create an account
+              創立新的帳號
             </h1>
           </div>
           {/* Contact form */}
@@ -24,13 +24,13 @@ export default function SignUp() {
                   className="mb-1 block text-sm font-medium text-indigo-200/65"
                   htmlFor="name"
                 >
-                  Name <span className="text-red-500">*</span>
+                  姓名 <span className="text-red-500">*</span>
                 </label>
                 <input
                   id="name"
                   type="text"
                   className="form-input w-full"
-                  placeholder="Your full name"
+                  placeholder="您的完整姓名"
                   required
                 />
               </div>
@@ -39,13 +39,13 @@ export default function SignUp() {
                   className="mb-1 block text-sm font-medium text-indigo-200/65"
                   htmlFor="name"
                 >
-                  Company Name <span className="text-red-500">*</span>
+                  公司名 <span className="text-red-500">*</span>
                 </label>
                 <input
                   id="company"
                   type="text"
                   className="form-input w-full"
-                  placeholder="Your company name"
+                  placeholder="您的公司名稱"
                   required
                 />
               </div>
@@ -54,13 +54,13 @@ export default function SignUp() {
                   className="mb-1 block text-sm font-medium text-indigo-200/65"
                   htmlFor="email"
                 >
-                  Work Email <span className="text-red-500">*</span>
+                  電子信箱 <span className="text-red-500">*</span>
                 </label>
                 <input
                   id="email"
                   type="email"
                   className="form-input w-full"
-                  placeholder="Your work email"
+                  placeholder="您的電子信箱"
                 />
               </div>
               <div>
@@ -68,33 +68,33 @@ export default function SignUp() {
                   className="block text-sm font-medium text-indigo-200/65"
                   htmlFor="password"
                 >
-                  Password <span className="text-red-500">*</span>
+                  密碼 <span className="text-red-500">*</span>
                 </label>
                 <input
                   id="password"
                   type="password"
                   className="form-input w-full"
-                  placeholder="Password (at least 10 characters)"
+                  placeholder="密碼 (最少 10 個字元)"
                 />
               </div>
             </div>
             <div className="mt-6 space-y-5">
               <button className="btn w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%]">
-                Register
+                註冊
               </button>
               <div className="flex items-center gap-3 text-center text-sm italic text-gray-600 before:h-px before:flex-1 before:bg-gradient-to-r before:from-transparent before:via-gray-400/25 after:h-px after:flex-1 after:bg-gradient-to-r after:from-transparent after:via-gray-400/25">
-                or
+                或
               </div>
               <button className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]">
-                Sign In with Google
+                使用 Google 帳號登入
               </button>
             </div>
           </form>
           {/* Bottom link */}
           <div className="mt-6 text-center text-sm text-indigo-200/65">
-            Already have an account?{" "}
+            已經有帳號了嗎？{" "}
             <Link className="font-medium text-indigo-500" href="/signin">
-              Sign in
+              登入
             </Link>
           </div>
         </div>
